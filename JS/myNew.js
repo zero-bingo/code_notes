1b1bdc3965212c92c315dede6c141b5848e87a30
zhaojiaming <18686712523@163.com>
2022-10-12 21:18:13 +0800

    feat: update

---
+++ JS/myNew.js
@@ -0,0 +1,13 @@
+const myNew = function (ctor, ...args) {
+    // 容错判断不止这一种，这里简写了
+    if (typeof ctor !== 'function') {
+        throw new Error()
+    }
+
+    const tempObj = new Object()
+    tempObj.__proto__ = Object.create(ctor.prototype)
+    const res = ctor.call(tempObj, ...args)
+
+    // 应该判断res的类型，这里简写了
+    return res
+}
